ed80b3f5b612f00f0cc5afdd5cf04790c37ae782
980rohan2702 <980rohan2702@gmail.com>
2025-08-20 01:25:26 +0530

    Week 7 - Intro to Testing , understanding JUnit testing and AsserJ library

--- src/test/java/com/codingshuttle/TestingApp/TestingAppApplicationTests.java
+++ src/test/java/com/codingshuttle/TestingApp/TestingAppApplicationTests.java
@@ -4,79 +4,75 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.*;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @Slf4j
 class TestingAppApplicationTests {
 
 	@BeforeEach
-	void setUp() {
-		log.info("Starting the method, setting up config");
+	void setUp(){
+		log.info("Setting the method, setting config");
 	}
 
 	@AfterEach
-	void tearDown() {
-		log.info("Tearing down the method");
+	void tearDown(){
+		log.info("Tearing Down the method...");
 	}
 
 	@BeforeAll
-	static void setUpOnce() {
-		log.info("Setup Once...");
+	static void setUpOnce(){
+		log.info("Setting up once");
 	}
 
 	@AfterAll
-    static void tearDownOnce() {
-		log.info("Tearing down all...");
+	static void tearDownOnce(){
+		log.info("Tearing Down once");
 	}
 
 	@Test
-//	@Disabled
-	void testNumberOne() {
+	//@Disabled - For disabling test Method
+	void testNumberOne(){
+		log.info("Test one running");
+
 		int a = 5;
 		int b = 3;
+		int result = addTwoNumbers(a,b);
 
-		int result = addTwoNumbers(a, b);
+		//assertions from Junit , cant use chaining , limited scope and functions
+		//Assertions.assertEquals(7,result);
 
-//		Assertions.assertEquals(8, result);
-//		Assertions.
-
-//		assertThat(result)
+//		Assertions.assertThat(result)
 //				.isEqualTo(7)
 //				.isCloseTo(9, Offset.offset(1));
 
-//		assertThat("Apple")
-//				.isEqualTo("Apple")
-//				.startsWith("App")
-//				.endsWith("l")
-//				.hasSize(3);
-
+		Assertions.assertThat("Apple")
+				.isEqualTo("Apple")
+				.startsWith("Apz")
+				.endsWith("e")
+				.hasSize(5);
 	}
 
 	@Test
-//	@DisplayName("displayTestNameTwo")
-	void testDivideTwoNumbers_whenDenominatorIsZero_ThenArithmeticException() {
-
+	//@DisplayName("displayNameTestTwo") - For Changing methodName
+	void testDivideTwoNumbers_whenDenominatorIsZero_ThenArithMeticExceptions(){
+		log.info("Test two running");
 		int a = 5;
-		int b = 0;
+		int b= 0;
 
-		assertThatThrownBy(() -> divideTwoNumbers(a, b))
-				.isInstanceOf(ArithmeticException.class)
-				.hasMessage("Tried to divide by zero");
+		Assertions.assertThatThrownBy(()->divideTwoNumbers(a,b))
+				.isInstanceOf(NullPointerException.class)
+				.hasMessage("Tried to divide By Zero");
 
 	}
 
-	int addTwoNumbers(int a, int b) {
+	int addTwoNumbers(int a , int b){
 		return a+b;
 	}
 
-	double divideTwoNumbers(int a, int b) {
+	double divideTwoNumbers(int a , int b){
 		try {
 			return a/b;
-		} catch (ArithmeticException e) {
-			log.error("Arithmentic excepiton occured: "+e.getLocalizedMessage());
-			throw new ArithmeticException("Tried to divide by zero");
+		}catch (ArithmeticException e){
+			log.error("Arithmetic exception occured: "+e.getLocalizedMessage());
+			throw new ArithmeticException("Tried to divide By Zero");
 		}
 	}
 
